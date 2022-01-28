83984b7038dde2678198e41b02651f0742df4496
brianliu8709 <brian.liu8709@gmail.com>
2022-01-28 12:40:54 -0500

    1/28

--- Project_Test/index.js
+++ Project_Test/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const lessons = require("./lessons.js")
 
 app.use(express.static( path.join(__dirname, 'public')))
 
@@ -8,19 +9,11 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
 app.get('/header.html', (req, res) => {
-    res.render('lessonTemplate.ejs', {
-        labName: "Headers",
-        instructions: "test instructions",
-        //criteria is a 2D array. list element id; regex; and written objective
-        
-        criteria: [['grade1', /<h2>.*<\/h2>/, "Create an h2 tag"], ['grade2', /<h3>.*<\/h3>/, "Create an h3 tag"], ['grade3', /<h6>.*<\/h6>/, "Create an h6 tag"]],
-        htmlStarterCode: "<h1>This is an h1 tag</h1>",
-        cssStarterCode: ""
-    })
+    res.render('lessonTemplate.ejs', lessons.header)
 })
 
 app.get('/paragraph.html', (req, res) => {
-    res.render('paragraph.ejs')
+    res.render('lessonTemplate.ejs', lessons.paragraph)
 })
 
 app.get('/index.html', (req, res) => {
